f052f56d24111adb656fa63b1f5a90c58fd07f6f
Mrxiaov <2282586126@qq.com>
2022-07-01 23:30:18 +0800

    chore: init

---
+++ src/App.test.tsx
@@ -0,0 +1,19 @@
+import { describe, expect, it } from 'vitest'
+import App from './App'
+import { render, screen, userEvent } from '@/utils/test-utils'
+
+describe('app should render', () => {
+  it('app render', () => {
+    render(<App />)
+
+    expect(screen.getByText(/Hello Vite \+ React!/i)).toBeInTheDocument()
+  })
+  it('button can click', async () => {
+    render(<App />)
+    const button = screen.getByRole('button')
+    expect(button).toBeInTheDocument()
+    expect(button).toHaveTextContent('count is: 0')
+    await userEvent.click(button)
+    expect(button).toHaveTextContent('count is: 1')
+  })
+})
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts'
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  }
+})
